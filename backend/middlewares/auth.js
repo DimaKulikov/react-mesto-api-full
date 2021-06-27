a151ab0cb39d9e499494bf036a4f2694bffbbeef
DimaKulikov <dmyukulikov@gmail.com>
2021-06-27 18:17:26 +0300

    fix: send 401 if no auth header present

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(ApiError.authorization());
+    next(ApiError.authentication('Требуется авторизация'));
     return;
   }
   const token = authorization.replace('Bearer ', '');
